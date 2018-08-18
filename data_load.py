d4a3c71f956f18d04da7fc21a6c6cbaf57e04ee0
Apratim Mukherjee <apratim101@gmail.com>
2018-08-19 01:14:40 +0530

    Added custom pytorch dataloader

---
+++ data_load.py
@@ -0,0 +1,31 @@
+import csv
+import torch
+import numpy as np
+from torch.autograd import Variable
+from torch.utils.data import DataLoader, Dataset
+
+class Sudoku(Dataset):
+	def __init__(self):
+		with open("sudoku.csv", "rb") as f:
+			reader = csv.reader(f, delimiter=",")
+			data = [[] for i in range(2)]
+			for i, line in enumerate(reader):
+				grid = np.array([line[0][i] for i in range(81)], dtype=int)
+				solve = np.array([line[1][i] for i in range(81)], dtype=int)
+				data[0].append(grid)
+				data[1].append(solve)
+
+		self.length = len(data[0])		
+		self.x_data = torch.Tensor(data[0])
+		self.y_data = torch.Tensor(data[1])
+
+	def __getitem__(self, index):
+		return self.x_data[index], self.y_data[index]
+
+	def __len__(self):
+		return self.length
+
+# data = Sudoku()
+# train_load = DataLoader(dataset=data, batch_size=4, shuffle=True)
+# dataiter = iter(train_load)
+# real, label = dataiter.next()
